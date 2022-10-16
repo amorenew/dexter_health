eaae9020f68d31d85e7b9e644f02e0321aa49a6d
Amr Eniou <amorenew@gmail.com>
2022-10-16 08:53:21 +0200

    fix google sign on chrome

--- lib/services/google_auth_web.dart
+++ lib/services/google_auth_web.dart
@@ -5,7 +5,10 @@ Future<User?> signInWithGoogle({
   required FirebaseAuth firebaseAuth,
   required GoogleSignIn googleSignIn,
 }) async {
-  final googleUser = await googleSignIn.signIn();
+  //I used sign silently to resolve the following issue
+  // https://github.com/flutter/flutter/issues/79442
+  GoogleSignInAccount? googleUser = await googleSignIn.signInSilently();
+  googleUser = await googleSignIn.signIn();
   final googleAuth = await googleUser!.authentication;
   final credential = GoogleAuthProvider.credential(
     accessToken: googleAuth.accessToken,
